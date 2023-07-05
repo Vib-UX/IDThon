0a8830c304b35cfb4b072fc1cc96274ec841377b
Ketan <krotusppp@gmail.com>
2023-07-06 04:09:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,6 +37,13 @@ const port = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("ngrok-skip-browser-warning", "true");
+  next();
+});
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
